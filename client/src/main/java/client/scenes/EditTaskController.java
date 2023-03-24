318f5d242bc98fe2ecc1bceef826012be87aeb86
Satsuki Udagawa <s.udagawa@student.tudelft.nl>
2023-03-24 17:59:38 +0100

    Fixed checkstyle errors

--- client/src/main/java/client/scenes/EditTaskController.java
+++ client/src/main/java/client/scenes/EditTaskController.java
@@ -4,7 +4,6 @@ import client.utils.ServerUtils;
 import commons.Task;
 import commons.models.IdResponseModel;
 import commons.models.TaskEditModel;
-import commons.sync.TaskEdited;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
