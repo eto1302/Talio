719e6e2d5db58131615efee44dc1a8641f546009
Satsuki Udagawa <s.udagawa@student.tudelft.nl>
2023-03-30 18:23:53 +0200

    And this is when test are uncommented

--- server/src/test/java/server/Services/TagServiceTest.java
+++ server/src/test/java/server/Services/TagServiceTest.java
@@ -1,76 +1,76 @@
-//package server.Services;
-//
-//import commons.Tag;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import server.api.TestBoardRepository;
-//import server.api.TestTagRepository;
-//import server.api.TestTaskRepository;
-//
-//import java.util.ArrayList;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class TagServiceTest {
-//
-//    private TestTagRepository tagRepo;
-//    private TestBoardRepository boardRepo;
-//    private TestTaskRepository taskRepo;
-//    private TagService tagService;
-//
-//    @BeforeEach
-//    void setUp() {
-//        tagRepo = new TestTagRepository();
-//        boardRepo = new TestBoardRepository();
-//        taskRepo = new TestTaskRepository();
-//        tagService = new TagService(tagRepo, taskRepo, boardRepo);
-//    }
-//
-//    @Test
-//    void getTagById() {
-//        var testTag = getTestTag("test");
-//        tagService.save(testTag);
-//        assertEquals(testTag, tagService.getTagById(testTag.getId()));
-//    }
-//
-//    @Test
-//    void getMissingTagById() {
-//        assertNull(tagService.getTagById(0));
-//    }
-//
-//    @Test
-//    void findAll() {
-//        var testTag = getTestTag("test");
-//        tagService.save(testTag);
-//        var expected = new ArrayList<Tag>();
-//        expected.add(testTag);
-//        assertEquals(expected, tagService.findAll());
-//    }
-//
-//    @Test
-//    void findAllEmpty() {
-//        assertEquals(new ArrayList<Tag>(), tagService.findAll());
-//    }
-//
-//    @Test
-//    void save() {
-//        var testTag = getTestTag("test");
-//        tagService.save(testTag);
-//        assertEquals(testTag, tagService.getTagById(testTag.getId()));
-//    }
-//
-//    @Test
-//    void count() {
-//        var testTag = getTestTag("test");
-//        tagService.save(testTag);
-//        var testTag1 = getTestTag("test");
-//        tagService.save(testTag);
-//        var testTag2 = getTestTag("test");
-//        tagService.save(testTag);
-//        assertEquals(3, tagService.count());
-//    }
-//
-//    private static Tag getTestTag(String q) {
-//        return Tag.create(q, "#000000");
-//    }
-//}
+package server.Services;
+
+import commons.Tag;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import server.api.TestBoardRepository;
+import server.api.TestTagRepository;
+import server.api.TestTaskRepository;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TagServiceTest {
+
+    private TestTagRepository tagRepo;
+    private TestBoardRepository boardRepo;
+    private TestTaskRepository taskRepo;
+    private TagService tagService;
+
+    @BeforeEach
+    void setUp() {
+        tagRepo = new TestTagRepository();
+        boardRepo = new TestBoardRepository();
+        taskRepo = new TestTaskRepository();
+        tagService = new TagService(tagRepo, taskRepo, boardRepo);
+    }
+
+    @Test
+    void getTagById() {
+        var testTag = getTestTag("test");
+        tagService.save(testTag);
+        assertEquals(testTag, tagService.getTagById(testTag.getId()));
+    }
+
+    @Test
+    void getMissingTagById() {
+        assertNull(tagService.getTagById(0));
+    }
+
+    @Test
+    void findAll() {
+        var testTag = getTestTag("test");
+        tagService.save(testTag);
+        var expected = new ArrayList<Tag>();
+        expected.add(testTag);
+        assertEquals(expected, tagService.findAll());
+    }
+
+    @Test
+    void findAllEmpty() {
+        assertEquals(new ArrayList<Tag>(), tagService.findAll());
+    }
+
+    @Test
+    void save() {
+        var testTag = getTestTag("test");
+        tagService.save(testTag);
+        assertEquals(testTag, tagService.getTagById(testTag.getId()));
+    }
+
+    @Test
+    void count() {
+        var testTag = getTestTag("test");
+        tagService.save(testTag);
+        var testTag1 = getTestTag("test");
+        tagService.save(testTag);
+        var testTag2 = getTestTag("test");
+        tagService.save(testTag);
+        assertEquals(3, tagService.count());
+    }
+
+    private static Tag getTestTag(String q) {
+        return Tag.create(q, "#000000");
+    }
+}
