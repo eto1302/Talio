55f3a389b4a0bcd44e299da8130c4647f22c3166
Cristiana Cotoi <C.cotoi@student.tudelft.nl>
2023-04-10 10:59:21 +0200

    Added styling to all scenes

--- client/src/main/java/client/scenes/TaskShape.java
+++ client/src/main/java/client/scenes/TaskShape.java
@@ -74,7 +74,7 @@ public class TaskShape {
         TaskShape selectedTask = controller.getBoardController().find();
         if (selectedTask==null) {
             selected = true;
-            grid.setStyle("-fx-border-color: rgba(14,27,111,1);" +
+            grid.setStyle("-fx-border-color: rgba(255, 255, 255, 1);" +
                 "-fx-border-width: 3px;" +
                 "-fx-background-color: " + taskColor.getBackgroundColor());
         }
@@ -117,7 +117,7 @@ public class TaskShape {
     public void setStatus(boolean selected){
         this.selected=selected;
         if (selected)
-            grid.setStyle("-fx-border-color: rgba(14,27,111,1);" +
+            grid.setStyle("-fx-border-color: rgba(255, 255, 255, 1);" +
                 "-fx-border-width: 3px;"+
                 "-fx-background-color: " + taskColor.getBackgroundColor());
         else grid.setStyle(style);
